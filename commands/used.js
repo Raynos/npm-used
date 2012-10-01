c7a438c29bcdc11cbd95fe3e261ad1bea4bfd400
Raynos <raynos2@gmail.com>
2012-09-30 23:48:05 -0700

    cleanup

--- commands/used.js
+++ commands/used.js
@@ -1,9 +1,16 @@
-var getDependenciesCount = require("../lib/getDependenciesCount")
-    , colors = require("colors")
+var colors = require("colors")
+
+    , getDependenciesCount = require("../lib/getDependenciesCount")
+    , getAuthors = require("../lib/getAuthors")
+    , DEFAULT = require("../lib/config").DEFAULT
 
 module.exports = used
 
 function used(category) {
+    if (!category) {
+        category = DEFAULT
+    }
+
     getDependenciesCount(category).toArray(function (list) {
         var last = list[list.length - 1]
 
@@ -25,16 +32,25 @@ function used(category) {
                 return str.indexOf("**dev**") === -1
             })
 
-        console.log("\n--------------------".cyan)
-        console.log("--------------------\n".cyan)
+        getAuthors(category, function (err, authors) {
+            console.log("\n------------------------------------".cyan)
+            console.log("------------------------------------\n".cyan)
 
-        console.log("Most used dependencies in", category.green, "\n")
-        console.log("A total of", last.length.toString().green
-            , "dependencies were found\n")
+            console.log("Most used dependencies in"
+                , category.green, "\n")
+            console.log("A total of", last.length.toString().green
+                , "dependencies were found\n")
 
-        console.log("Popular Dependencies"
-            , "\n\n\t" + notDev.slice(0, 15).join("\n\t"), "\n")
-        console.log("Popular DevDependencies"
-            , "\n\n\t" + dev.slice(0, 15).join("\n\t"), "\n")
+            console.log("Based on the following authors:\n")
+
+            for (var i = 0; i < authors.length; i++) {
+                console.log("\t -", authors[i].cyan)
+            }
+
+            console.log("\nPopular Dependencies"
+                , "\n\n\t" + notDev.slice(0, 15).join("\n\t"), "\n")
+            console.log("Popular DevDependencies"
+                , "\n\n\t" + dev.slice(0, 15).join("\n\t"), "\n")
+        })
     })
 }
